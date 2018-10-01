934ebc72024992f9c4d0d11e8213c5dba8fc4490
Hamed <hameds@gmail.com>
2018-10-01 11:24:29 +0330

    initial commit

---
+++ Textmining.Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using Textmining.Demo.Web.Models;
+
+namespace Textmining.Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FormalConverter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult FormalConverter(ApiViewModel model)
+        {
+            model.OutputText = CallApi("http://api.text-mining.ir/api/PreProcessing/FormalConverter", model.InputText);
+            return View(model);
+        }
+
+        public IActionResult SpellCorrector()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SpellCorrector(ApiViewModel model)
+        {
+            model.OutputText = CallApi("http://api.text-mining.ir/api/PreProcessing/SpellCorrector", model.InputText);
+            return View(model);
+        }
+
+        private string GetJWTToken()
+        {
+            var textMiningApiKey = _config.GetValue<string>("TextMiningApiKey");
+            string jwtToken = string.Empty;
+            var client = new RestClient($"http://api.text-mining.ir/api/Token/GetToken?apikey={textMiningApiKey}");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Cache-Control", "no-cache");
+            IRestResponse response = client.Execute(request);
+            if (response.ResponseStatus == ResponseStatus.Completed && response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                jwtToken = (string)JObject.Parse(response.Content)["token"];
+            }
+            return jwtToken;
+        }
+
+        private string CallApi(string apiUrl,string inputText)
+        {
+            try
+            {
+                //ToDo: check model validation
+                var client = new RestClient(apiUrl);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Cache-Control", "no-cache");
+                request.AddHeader("Authorization", $"Bearer {GetJWTToken()}");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("input", $"\"{inputText}\"", ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                return response.Content.Replace("\"", string.Empty);
+            }
+            catch (Exception ex)
+            {
+                //ToDo: better error message
+                return "Error";
+            }
+        }
+    }
+}
---
+++ Textmining.Demo.Web/Models/ApiViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Textmining.Demo.Web.Models
+{
+    public class ApiViewModel
+    {
+        [Required(ErrorMessage ="الزامی")]
+        public string InputText { get; set; }
+        public string OutputText { get; set; }
+    }
+}
